31c6dad9fc52161c6bc2f4ceca4ef464e8a4bd6a
Sa Aung Htet Nyein <saaunghtetnyein711@gmail.com>
2026-09-20 18:41:01 +0700

    feat: compose home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
-import { Suspense } from "react";
+import { ExploreMore } from "@/components/sections/explore-more";
+import { Hero } from "@/components/sections/hero";
+import { Process } from "@/components/sections/process";
 import { Testimonials } from "@/components/sections/testimonials";
+import { WhyChoose } from "@/components/sections/why-choose";
 
 export default function Home() {
   return (
-    <Suspense>
+    <>
+      <Hero />
+      <Process />
+      <WhyChoose />
+      <ExploreMore />
       <Testimonials />
-    </Suspense>
+    </>
   );
 }
